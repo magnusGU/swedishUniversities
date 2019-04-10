64b4d4574b9a617016779c320a9575afa0bb052c
k <enlitengas@gmail.com>
2019-04-10 15:40:46 +0200

    lala

---
+++ getdata.py
@@ -0,0 +1,30 @@
+# pip install sparqlwrapper
+# https://rdflib.github.io/sparqlwrapper/
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """#Cats
+SELECT ?uniLabel ?year
+WHERE 
+{
+  ?uni wdt:P31 wd:Q3918;
+       wdt:P17 wd:Q34.
+  ?uni wdt:P571 ?year.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}"""
+
+
+def get_results(endpoint_url, query):
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+
+for result in results["results"]["bindings"]:
+    print(result)
+    
