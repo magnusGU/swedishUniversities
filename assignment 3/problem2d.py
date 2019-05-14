8dd17b7a026afed048099e0db0742c6ad2e38981
Magnus Andersson <gusbannbma@student.gu.se>
2019-05-14 10:01:57 +0200

    Cleanup dead code

--- assignment 3/problem2d.py
+++ assignment 3/problem2d.py
@@ -12,15 +12,11 @@ from sklearn.datasets import make_blobs
 import time
 import multiprocessing
 import ctypes
-from multiprocessing.sharedctypes import RawArray
 import time
-
 
 var_dict = {}
 
 def initProcess(data,c):
-    #time.data = data
-    #var_dict['c'] = c
     var_dict['data'] = data
     var_dict['c'] = c
 
@@ -40,11 +36,9 @@ def nearestCentroid(datum, centroids):
 
 def assignment(start,fin,k,centroids):
     variation = np.zeros(k)
-    #cluster_sizes = np.zeros(k, dtype=int)
     for i in range(start,fin):
         cluster, dist = nearestCentroid(var_dict['data'][i],centroids)
         var_dict['c'][i] = cluster
-        ##cluster_sizes[cluster] += 1
         variation[cluster] += dist**2
     return variation
 
@@ -63,48 +57,34 @@ def kmeans(k, data, nr_iter = 100, workers = 1):
     centroids = [data[0],data[44],data[4]]#data[np.random.choice(np.array(range(N)),size=k,replace=False)]
     logging.debug("Initial centroids\n", centroids)
 
-    N = len(data)
-    #print(N)
-    #cc = RawArray(ctypes.c_int,N)
-    #c = np.frombuffer(cc,dtype=int)    
+    N = len(data) 
     c = multiprocessing.Array('i',N)
     pool = multiprocessing.Pool(initializer=initProcess,initargs=(data,c,),processes=workers)
 
 
     # The cluster index: c[i] = j indicates that i-th datum is in j-th cluster
-    #c = np.zeros(N, dtype=int)
     timing = [0,0]
     logging.info("Iteration\tVariation\tDelta Variation")
     total_variation = 0.0
 
-    #argumentlist = argumentListMaker(N,workers,data,centroids)
     for j in range(nr_iter):
         
         logging.debug("=== Iteration %d ===" % (j+1))
 
         start = time.time()
         # Assign data points to nearest centroid
-        #print(c[:])
         s = pool.starmap(assignment,[(i*(N//workers),(i+1)*(N//workers),k,centroids) for i in range(workers)])
-        #print(c[:])
         
         timing[0] += time.time() - start
         total_variation = np.zeros(k,dtype=float)
         cluster_sizes = np.zeros(k,dtype=int)
         for i in range(len(c)):
                 cluster_sizes[c[i]] += 1
-        #delta_variation = -total_variation
- #       total_variation = sum(variation) 
-        #delta_variation += total_variation
-        #logging.info("%3d\t\t%f\t%f" % (j, total_variation, delta_variation))
-        #print(delta_variation, total_variation)
 
         start = time.time()
         # Recompute centroids
         centroids = np.zeros((k,2)) # This fixes the dimension to 2
-
-        #for i in range(N):
-        #    centroids[c[i]] += data[i]        
+     
         s = pool.starmap(computeCentroids,[(i*(N//workers),(i+1)*(N//workers),k) for i in range(workers)])
        
 
