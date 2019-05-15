53db5c77b7309d3a9c22b8f211f096c97c512f39
Magnus Andersson <gusbannbma@student.gu.se>
2019-05-15 14:38:12 +0200

    Here

--- lab4/problem1a.py
+++ lab4/problem1a.py
@@ -10,31 +10,70 @@ class MRWordFrequencyCount(MRJob):
         #yield "group", data[1]
         yield "value", float(value)
 
+    def __looper__(self, values):
+        l = []
+        total = 0
+        
+        return l.sort(), total, i
 
     def combiner(self, key, values):
         total = 0
         l = []
+        #l, total, i = self.__looper__(values)
         for i, v in enumerate(values):
             l.append(v)
             total += v
         l.sort()
         interval = (l[-1] - l[0]) / 10
-        thres = l[0] + interval
+        thres = l[0]# + interval
         d = defaultdict(float)
 		#
         for e in l:
-            if e > thres:
+            if e > thres + interval:
                 thres += interval
             d[thres] += 1
         
-        yield ("dict", d)
+        yield ("dict", (d,l[0],l[-1]))
         yield ("count", i)
         yield ("total",total)
         yield ("min", l[0])
         yield ("max", l[-1])
         
     def reducer(self, key, values):
-        yield "dead", True
+        if key == 'dict':
+            d = []
+            l = []
+            for i, v in enumerate(values):
+                d.append(v[0])
+                l.append(v[1])
+                l.append(v[-1])
+            l.sort()
+            
+            interval = (l[-1] - l[0]) / 10
+            thres = l[0] #+ interval
+            finalD = defaultdict(float)
+            for dic in d:
+                for key in dic:
+                    if float(key) > thres + interval:
+                        thres += interval
+                    finalD[thres] += dic[key]
+            for key in finalD:
+                yield key, finalD[key]
+            yield 'min', l[0]
+            yield 'max', l[-1]
+
+        #if key == "min":
+        #    l = []
+        #    for i, v in enumerate(values):
+        #        l.append(v)
+        #    l.sort()
+        #    yield key, l[0]
+        #if key == "max":
+        #    l = []
+        #    for i, v in enumerate(values):
+        #        l.append(v)
+        #    l.sort()
+        #    yield key, l[-1]
         
 
 
