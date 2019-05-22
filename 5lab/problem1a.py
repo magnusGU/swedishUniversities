1472360548ff9bae6f05abacbadbd7c32124a4e8
klas <klas@ludvigsson.se>
2019-05-22 14:34:32 +0200

    lal

--- 5lab/problem1a.py
+++ 5lab/problem1a.py
@@ -1,24 +1,31 @@
+import random
 from pyspark import SparkContext
+sc = SparkContext(master = 'local[4]')
 
-def run():
-    sc = SparkContext(master = 'local[4]')
-    #sc.setLogLevel(0)
-    quiet_logs(sc)
-    distFile = sc.textFile("datasnippet.txt")
+distFile = sc.textFile("datasnippet.txt")
+splitData = distFile.map(lambda l : l.split()).map(lambda t:float(t[2]))
+count = splitData.count()
+_sum = splitData.reduce(lambda a,b: a+b)
+_expsum = splitData.map(lambda a: a**2).reduce(lambda a,b:a+b)
+median = -1
+ordered = splitData.takeOrdered(count)
+if count % 2 == 0:
+    median = (ordered[count//2] + ordered[count//2 - 1]) / 2
+else:
+    median = ordered[count//2]
+_min = ordered[0]
+_max = ordered[count-1]
 
-    counts = distFile.map(lambda l: l.split('\t')).map(lambda t:(int(t[0]),1)).reduceByKey(lambda a, b: a + b).filter(lambda t:t[1] > 1)
+interval = (_max - _min)
+histogram = splitData.map(lambda a: (int(((a-_min)/interval)*10),1)).reduceByKey(lambda a,b:a+b)
 
-    print(counts)
-    cc = counts.collect()
-
-    print("*******************************************************************")
-    print(cc)
-
-def quiet_logs( sc ):
-  logger = sc._jvm.org.apache.log4j
-  logger.LogManager.getLogger("org"). setLevel( logger.Level.OFF )
-  logger.LogManager.getLogger("akka").setLevel( logger.Level.OFF )
-
-
-if __name__ == '__main__':
-    run()
+hg = histogram.collect()
+avg = _sum / count
+std = (_expsum/count - avg**2)**(0.5)
+print("Count: ",count)
+print("Avg: ",avg)
+print("Standard deviation: ",std)
+print("Median: ",median)
+print("Min: ",_min)
+print("Max: ",_max)
+print(hg)
